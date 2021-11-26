77eaa0d52c3e46b86c3e3c96580e9be86dc99532
Shikhar Agrawal <200070076@iitb.ac.in>
2021-11-26 19:33:29 +0530

    Tokens made, Location, Date, Time done! Databasing left

--- app/src/main/java/com/example/covidapp/ProfileActivity.java
+++ app/src/main/java/com/example/covidapp/ProfileActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,9 +17,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText name_edit,gender_edit,phone_number_edit,age_edit;
+    EditText name_edit,gender_edit,phone_number_edit,age_edit, mac_address_edit;
     TextView save;
     FirebaseAuth auth;
     FirebaseUser user;
@@ -36,6 +38,7 @@ public class ProfileActivity extends AppCompatActivity {
         phone_number_edit=findViewById(R.id.phone_edit_text);
         age_edit=findViewById(R.id.age_edit_text);
         questionairre=findViewById(R.id.questionairre);
+        mac_address_edit = findViewById(R.id.mac_address_edit);
 
         save=findViewById(R.id.save);
 
@@ -48,6 +51,7 @@ public class ProfileActivity extends AppCompatActivity {
         user=auth.getCurrentUser();
 
         reference= FirebaseDatabase.getInstance().getReference().child("users");
+
         reference.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -56,11 +60,13 @@ public class ProfileActivity extends AppCompatActivity {
                     String gender = snapshot.child("gender").getValue(String.class);
                     String age = snapshot.child("age").getValue(String.class);
                     String phone_number = snapshot.child("phone_number").getValue(String.class);
+                    String mac_address = snapshot.child("mac_address").getValue(String.class);
 
                     name_edit.setText(name);
                     gender_edit.setText(gender);
                     phone_number_edit.setText(phone_number);
                     age_edit.setText(age);
+                    mac_address_edit.setText(mac_address);
                 }
             }
             @Override
@@ -72,20 +78,31 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void save_data() {
+        String regex = "^([0-9A-Fa-f]{2}[:-])"
+                + "{5}([0-9A-Fa-f]{2})|"
+                + "([0-9a-fA-F]{4}\\."
+                + "[0-9a-fA-F]{4}\\."
+                + "[0-9a-fA-F]{4})$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(mac_address_edit.getText().toString());
         if(!name_edit.getText().toString().equals("")){
             if(!gender_edit.getText().toString().equals("")){
                 if(!phone_number_edit.getText().toString().equals("") && phone_number_edit.getText().toString().length()==10){
                     if(!age_edit.getText().toString().equals("")){
-
-                        reference.child(user.getUid()).child("name").setValue(name_edit.getText().toString());
-                        reference.child(user.getUid()).child("gender").setValue(gender_edit.getText().toString());
-                        reference.child(user.getUid()).child("phone_number").setValue(phone_number_edit.getText().toString());
-                        reference.child(user.getUid()).child("age").setValue(age_edit.getText().toString());
-                        Intent info_dashboard=new Intent(ProfileActivity.this, Info_dashboard.class);
-                        startActivity(info_dashboard);
-                        finish();
-                        Toast.makeText(ProfileActivity.this, "Changes saved successfully.", Toast.LENGTH_SHORT).show();
-
+                        if(m.matches()){
+                            reference.child(user.getUid()).child("name").setValue(name_edit.getText().toString());
+                            reference.child(user.getUid()).child("gender").setValue(gender_edit.getText().toString());
+                            reference.child(user.getUid()).child("phone_number").setValue(phone_number_edit.getText().toString());
+                            reference.child(user.getUid()).child("age").setValue(age_edit.getText().toString());
+                            reference.child(user.getUid()).child("mac_address").setValue(mac_address_edit.getText().toString());
+                            Intent info_dashboard=new Intent(ProfileActivity.this, Info_dashboard.class);
+                            startActivity(info_dashboard);
+                            finish();
+                            Toast.makeText(ProfileActivity.this, "Changes saved successfully.", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(ProfileActivity.this, "Please enter the correct MAC Address of your device", Toast.LENGTH_SHORT).show();
+                        }
                     }
                     else{
                         Toast.makeText(ProfileActivity.this, "Please enter your age", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/covidapp/bluetoothDiscovery.java
+++ app/src/main/java/com/example/covidapp/bluetoothDiscovery.java
@@ -1,41 +1,87 @@
 package com.example.covidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 
-public class bluetoothDiscovery extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class bluetoothDiscovery extends AppCompatActivity implements LocationListener {
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    String lat;
+    String provider;
+    protected String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    DatabaseReference reference;
 
     BluetoothAdapter bluetoothAdapter;
     ListView bluetoothListView;
     ArrayAdapter<String> bluetoothAdp;
 
-    ArrayList bluetoothList;
-
+    ArrayList<String> bluetoothList;
+    ArrayList<BluetoothDevice> mDeviceList;
     Button scanBtn;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_discovery);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference().child("users");
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothListView = (ListView) findViewById(R.id.bluetoothDevices);
-        bluetoothList = new ArrayList();
-
+        bluetoothList = new ArrayList<String>();
+        mDeviceList = new ArrayList<BluetoothDevice>();
         bluetoothAdp = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, bluetoothList);
         bluetoothListView.setAdapter(bluetoothAdp);
 
@@ -43,31 +89,69 @@ public class bluetoothDiscovery extends AppCompatActivity {
         scanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("Scan Button clicked");
+                bluetoothList.clear();
                 discoverDevices(v);
             }
         });
-
     }
 
-    public void discoverDevices(View v)
-    {
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = ""+location.getLatitude();
+        longitude = ""+location.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
+    public void discoverDevices(View v){
         bluetoothAdapter.startDiscovery();
+        System.out.println("Bluetooth Discovery has started");
     }
 
     BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            System.out.println("Received a request");
+
             String action = intent.getAction();
 
-            if (BluetoothDevice.ACTION_FOUND.equals(action))
-            {
+            if (BluetoothDevice.ACTION_FOUND.equals(action)){
+                Date current_Date = new Date();
                 BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                bluetoothList.add(bluetoothDevice.getName());
-                bluetoothAdp.notifyDataSetChanged();
-            }
+                Token currentToken = new Token();
+                currentToken.setDATE(current_Date.toString());
+                currentToken.setLONGITUDE(longitude);
+                currentToken.setLATITUDE(latitude);
+                currentToken.setMAC_ADDRESS(bluetoothDevice.getAddress());
 
+                reference.child(user.getUid()).child("Token").setValue(currentToken);
+//                System.out.println("The latitude is: "+currentToken.LATITUDE);
+//                System.out.println("The longitude is: "+currentToken.LONGITUDE);
+//                System.out.println("The Date is: "+currentToken.DATE);
+//                System.out.println("The MAC Address is: "+currentToken.MAC_ADDRESS);
+
+                bluetoothList.add(bluetoothDevice.getName());
+                mDeviceList.add(bluetoothDevice);
+                bluetoothAdp.notifyDataSetChanged();
+                System.out.println("A device was found!"+bluetoothDevice.getName());
+            }
         }
     };
+
 
     @Override
     protected void onStart() {
